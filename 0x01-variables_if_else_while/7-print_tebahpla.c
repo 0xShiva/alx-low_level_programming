bec793aeaa7e130fb74a007debac6c79452270d0
0xShiva <samie.elbra@gmail.com>
2023-09-10 18:38:46 +0000

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * displays alphabet in lower_case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0x7A;
+	while (n >= 0x61 && n <= 0x7A)
+		putchar(n--);
+	putchar(0xA);
+	return (0);
+}
+
