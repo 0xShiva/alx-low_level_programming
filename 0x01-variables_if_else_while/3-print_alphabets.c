2c52dac12ebbe998d691904e741b90cae130cdf5
0xShiva <samie.elbra@gmail.com>
2023-09-10 18:24:44 +0000

    alPHA

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-  
+
 /**
  * main - Entry point
  * displays alphabet in lower_case
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-        int n;
+	int n;
 
-        n = 0x61;
-        while (n >= 0x61 && n <= 0x7A)
-                putchar(n++);
-	n = 0x41;
-        while (n >= 0x41 && n <= 0x5A)
+	n = 0x61;
+	while (n >= 0x61 && n <= 0x7A)
 		putchar(n++);
-        putchar(0xA);
+	n = 0x41;
+	while (n >= 0x41 && n <= 0x5A)
+		putchar(n++);
+	putchar(0xA);
 	return (0);
 }
 
