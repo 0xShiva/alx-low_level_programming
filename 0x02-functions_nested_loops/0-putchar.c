80989fd787793331437fb4427423ecc49ccdb35c
0xShiva <samie.elbra@gmail.com>
2023-09-12 17:49:37 +0000

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - print string
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(1, "_putchar\n", 9);
+	return (0);
+}
+
