429de625cedc5db92ea108cfc8d9bf31ed17ea37
0xShiva <samie.elbra@gmail.com>
2023-09-13 11:10:12 +0000

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * print_sign - checks the sign of number
+ * @n: number to check
+ * Return: 1 if sign pos 0 if n = 0 and -1 if sign is nigative
+ */
+
+int	print_sign(int n)
+{
+	if (!n)
+	{
+		_putchar(0x30);
+		return (n);
+	}
+	else if (n > 0)
+	{
+		_putchar(0x2B);
+		return (0x31);
+	}
+	else
+	{
+		_putchar(0x2D);
+		return (-1);
+	}
+}
+
