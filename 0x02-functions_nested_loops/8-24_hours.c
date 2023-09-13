85f39f4bc5fcf297b8a138553bb232890d402b7e
0xShiva <samie.elbra@gmail.com>
2023-09-13 12:29:43 +0000

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * jack_bauer - prints every minute of the day
+ * 
+ */
+
+void jack_bauer(void)
+{
+	int	hour;
+	int	min;
+
+	hour = -1;
+	while (++hour < 0x18)
+	{
+		min = -1;
+		while (++min < 0x3C)
+		{
+			_putchar((hour / 0xA) + 0x30);
+			_putchar((hour % 0xA) + 0x30);
+			_putchar(':');
+			_putchar((min / 0xA) + 0x30);
+			_putchar((min % 0xA) + 0x30);
+			_putchar(0xA);
+		}
+
+	}
+}
