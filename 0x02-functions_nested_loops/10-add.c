63c155920f260b0efd62390604a9a493adea1077
0xShiva <samie.elbra@gmail.com>
2023-09-13 13:03:09 +0000

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * add - sum up the value of two integers
+ * @x: value of integer
+ * @y: value of integer
+ * Return: sum of two values
+ */
+int	add(int x, int y)
+{
+	return (x + y);
+}
+
