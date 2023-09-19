baea400e34649567912c1a4ed70519b68e3d79c9
0xShiva <samie.elbra@gmail.com>
2023-09-19 00:59:30 +0000

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/* betty style doc for function main goes there */
+/**
+ * _strcpy - copy src string in dest string
+ * @dest: string to copy to
+ * @n: string to copy from
+ * Return: pointer to dest string
+ */
+
+char	*_strcpy(char *dest, char *src)
+{
+	int	idx, i;
+
+	idx = -1;
+	while (src[++idx])
+		;
+	i = -1;
+	while (++i < idx)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
